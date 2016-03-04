33b6abe8db100b74226dc3aec1507267c90c3ec5
Lê Hùng Thiện <hungthien9394@gmail.com>
2016-03-04 19:50:50 +0700

    Add new projects

--- OMDbExample/app/src/main/java/thienle/sample/com/omdbexample/MainActivity.java
+++ OMDbExample/app/src/main/java/thienle/sample/com/omdbexample/MainActivity.java
@@ -28,17 +28,17 @@ import java.net.URLEncoder;
 import thienle.sample.com.omdbexample.pojo.Movie;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+}
 
-    /**
-     *
-     * @param view
-     */
-    public void getMovieInfo(View view) {
+/**
+ *
+ * @param view
+ */
+public void getMovieInfo(View view) {
         //  Get the text
         EditText editText = (EditText) findViewById(R.id.promt_title);
         String movieTitle = editText.getText().toString();
@@ -46,127 +46,127 @@ public class MainActivity extends AppCompatActivity {
         String url = "http://www.omdbapi.com/?%s=%s";
 
         try {
-            String urlString = String.format(url, "t", URLEncoder.encode(movieTitle, "UTF-8"));
-            new BackgroundTask(this).execute(urlString);
+                String urlString = String.format(url, "t", URLEncoder.encode(movieTitle, "UTF-8"));
+                new BackgroundTask(this).execute(urlString);
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+                e.printStackTrace();
         }
-    }
+}
 
-    /**
-     *
-     */
-    private class BackgroundTask extends AsyncTask<String, Void, Movie> {
-        private final ProgressDialog dialog;
-        private HttpURLConnection connection;
-        private Bitmap bitmap;
-        private Context context;
+/**
+ *
+ */
+private class BackgroundTask extends AsyncTask<String, Void, Movie> {
+private final ProgressDialog dialog;
+private HttpURLConnection connection;
+private Bitmap bitmap;
+private Context context;
 
-        public BackgroundTask(Context context) {
-            this.context = context;
-            dialog = new ProgressDialog(context);
-        }
+public BackgroundTask(Context context) {
+        this.context = context;
+        dialog = new ProgressDialog(context);
+}
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+@Override
+protected void onPreExecute() {
+        super.onPreExecute();
 
-            ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-            if (manager == null) {
+        if (manager == null) {
                 Toast.makeText(context, "Not connected to the Internet", Toast.LENGTH_SHORT).show();
                 this.cancel(true);
-            } else {
+        } else {
                 this.dialog.setMessage("Wait..for...it...");
                 this.dialog.show();
-            }
         }
+}
 
-        private Bitmap loadImage(String url) throws IOException {
-            Bitmap bitmap;
+private Bitmap loadImage(String url) throws IOException {
+        Bitmap bitmap;
 //            inputStream = new URL(url).openStream();
-            InputStream inputStream = this.openHttpConnection(url);
-            bitmap = BitmapFactory.decodeStream(inputStream);
-            inputStream.close();
-            return bitmap;
-        }
+        InputStream inputStream = this.openHttpConnection(url);
+        bitmap = BitmapFactory.decodeStream(inputStream);
+        inputStream.close();
+        return bitmap;
+}
 
-        private Movie readAndParseJSON(String input) {
-            Movie movie = null;
-            try {
+private Movie readAndParseJSON(String input) {
+        Movie movie = null;
+        try {
                 JSONObject jsonMovie = new JSONObject(input);
 
                 if (jsonMovie.getString("Response").equals("True")) {
-                    movie = new Movie();
-                    movie.setTitle(jsonMovie.getString("Title"));
-                    movie.setYear(jsonMovie.getString("Year"));
-                    movie.setActors(jsonMovie.getString("Actors"));
-                    movie.setAwards(jsonMovie.getString("Awards"));
-                    movie.setPoster(jsonMovie.getString("Poster"));
-                    movie.setDirector(jsonMovie.getString("Director"));
-                    movie.setPlot(jsonMovie.getString("Plot"));
-                    movie.setImdbRating(jsonMovie.getString("imdbRating"));
+                        movie = new Movie();
+                        movie.setTitle(jsonMovie.getString("Title"));
+                        movie.setYear(jsonMovie.getString("Year"));
+                        movie.setActors(jsonMovie.getString("Actors"));
+                        movie.setAwards(jsonMovie.getString("Awards"));
+                        movie.setPoster(jsonMovie.getString("Poster"));
+                        movie.setDirector(jsonMovie.getString("Director"));
+                        movie.setPlot(jsonMovie.getString("Plot"));
+                        movie.setImdbRating(jsonMovie.getString("imdbRating"));
                 }
-            } catch (JSONException e) {
+        } catch (JSONException e) {
                 e.printStackTrace();
-            }
-
-            return movie;
         }
 
-        /**
-         * @param sUrl
-         * @return
-         * @throws IOException
-         */
-        private InputStream openHttpConnection(String sUrl) throws IOException {
-            InputStream inputStream;
-            int response;
+        return movie;
+}
 
-            URL url = new URL(sUrl);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);   //  Use for GET and POST
-            connection.setAllowUserInteraction(true);
-            connection.setInstanceFollowRedirects(true);
-            connection.connect();
-            response = connection.getResponseCode();
+/**
+ * @param sUrl
+ * @return
+ * @throws IOException
+ */
+private InputStream openHttpConnection(String sUrl) throws IOException {
+        InputStream inputStream;
+        int response;
 
-            if (response == HttpURLConnection.HTTP_OK) {    //  200 :v
+        URL url = new URL(sUrl);
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setDoInput(true);       //  Use for GET and POST
+        connection.setAllowUserInteraction(true);
+        connection.setInstanceFollowRedirects(true);
+        connection.connect();
+        response = connection.getResponseCode();
+
+        if (response == HttpURLConnection.HTTP_OK) {        //  200 :v
                 inputStream = connection.getInputStream();
                 return inputStream;
-            }
-
-            return null;
         }
 
-        @Override
-        protected Movie doInBackground(String... params) {
-            Movie movie = null;
-            try {
+        return null;
+}
+
+@Override
+protected Movie doInBackground(String ... params) {
+        Movie movie = null;
+        try {
 //                InputStream inputStream = new URL(params[0]).openStream();
                 InputStream inputStream = openHttpConnection(params[0]);
                 if (inputStream != null) {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                    movie = readAndParseJSON(reader.readLine());
-                    reader.close();
-                    inputStream.close();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                        movie = readAndParseJSON(reader.readLine());
+                        reader.close();
+                        inputStream.close();
 
-                    if (movie != null) {
-                        //  Load the poster
-                        bitmap = loadImage(movie.getPoster());
-                    }
+                        if (movie != null) {
+                                //  Load the poster
+                                bitmap = loadImage(movie.getPoster());
+                        }
                 }
-            } catch (IOException e) {
+        } catch (IOException e) {
                 e.printStackTrace();
-            }
-
-            return movie;
         }
 
-        @Override
-        protected void onPostExecute(Movie movie) {
+        return movie;
+}
+
+@Override
+protected void onPostExecute(Movie movie) {
 //            super.onPostExecute(movie);
-            if (movie != null) {
+        if (movie != null) {
                 //  Set the views here!!!
                 Intent intent = new Intent(context, MovieInfoActivity.class);
                 intent.putExtra("movie_title", movie.getTitle());
@@ -178,21 +178,21 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("movie_plot", movie.getPlot());
 
                 if (bitmap != null) {
-                    intent.putExtra("movie_poster", bitmap);
+                        intent.putExtra("movie_poster", bitmap);
                 }
 
                 startActivity(intent);
-            } else {
+        } else {
                 Toast.makeText(context, "Movie not found!", Toast.LENGTH_SHORT).show();
-            }
-
-            if (connection != null) {
-                connection.disconnect();
-            }
-
-            if (this.dialog.isShowing()) {
-                this.dialog.dismiss();
-            }
         }
-    }
+
+        if (connection != null) {
+                connection.disconnect();
+        }
+
+        if (this.dialog.isShowing()) {
+                this.dialog.dismiss();
+        }
+}
+}
 }
